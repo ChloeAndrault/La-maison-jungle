4b0b7c112022aa10c96f1ce1a101b3adbfbfd428
Chloé Andrault <candrault@indexel.com>
2021-06-14 12:18:40 +0200

    Init project

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import Banner from './Banner';
+import Description from './Description';
+import Cart from './Cart';
+
+function App(){
+
+  return(
+    <div>
+      <Banner />
+      <Description />
+      <Cart />
+    </div>
+  )
+
+}
+
+export default App;
